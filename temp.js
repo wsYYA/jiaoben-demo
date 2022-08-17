486be33d10d67fcb7818e66086a27a21b99097a8
wsYYA <110587009+wsYYA@users.noreply.github.com>
2022-08-17 11:01:11 +0800

    Create temp.js

---
+++ temp.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Hello world!!!
+// @namespace    http://wfyljj.xyz/
+// @version      0.1
+// @description  new script
+// @author       YYA
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bing.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    console.log("Hello world!!!");
+})();
